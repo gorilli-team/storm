049800d0f66f092dd2147b18d52fe677f87668da
Paolo Antonio Rossi <paolo@luduvigo.com>
2025-04-09 23:16:30 +0200

    update

---
+++ mcp-demo/src/index.ts
@@ -0,0 +1,30 @@
+import { WorkerEntrypoint } from 'cloudflare:workers';
+import { ProxyToSelf } from 'workers-mcp';
+
+interface Env {
+	SHARED_SECRET: string;
+}
+
+export default class MyWorker extends WorkerEntrypoint<Env> {
+	/**
+	 * A warm, friendly greeting from your new Workers MCP server.
+	 * @param name {string} the name of the person we are greeting.
+	 * @return {string} the contents of our greeting.
+	 */
+	sayHello(name: string) {
+		return `Hello from an MCP Worker, ${name}!`;
+	}
+
+	getWeather(city: string) {
+		return `The weather in ${city} is sunny`;
+	}
+
+	get
+
+	/**
+	 * @ignore
+	 **/
+	async fetch(request: Request): Promise<Response> {
+		return new ProxyToSelf<Env>(this).fetch(request);
+	}
+}
